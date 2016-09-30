916c2e277cc3e7e0ddb07fb93a362aaf54cc44db
adricen <adrien.centonze@gmail.com>
2016-09-30 11:53:14 +0200

    premier commit afin de verifier la synthax du fichier

---
+++ XmasTree-01/inc/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Xmas Tree</title>
+</head>
+<body>
+	<div>
+		<h1>Xmas Tree</h1>
+		<form methode="get" action="sapin.php" >
+			<label for="nombre">Combien voulez vous d'étage à votre sapin?</label>
+			<input type="text" name="nb">
+		</form>	
