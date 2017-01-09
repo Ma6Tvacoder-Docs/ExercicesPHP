05553f13a050e30b01210cae3925832dad759d86
Gootime <kevin.cheruel@lepoles.org>
2017-01-09 14:00:10 +0100

    Ajout .sql

--- [db]trombi/trombinoschool.sql
+++ [db]trombi/trombinoschool.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 20 Décembre 2016 à 10:31
--- Version du serveur :  10.1.13-MariaDB
--- Version de PHP :  5.6.23
+-- Généré le :  Lun 09 Janvier 2017 à 13:59
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `trombinoschool`
@@ -26,10 +26,30 @@ SET time_zone = "+00:00";
 -- Structure de la table `bouche`
 --
 
-CREATE TABLE `bouche` (
-  `id_bouche` int(11) NOT NULL,
-  `img_bouche` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `bouche` (
+  `id_bouche` int(11) NOT NULL AUTO_INCREMENT,
+  `img_bouche` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_bouche`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=367 ;
+
+--
+-- Contenu de la table `bouche`
+--
+
+INSERT INTO `bouche` (`id_bouche`, `img_bouche`) VALUES
+(1, 'bouche-00.png'),
+(2, 'bouche-01.png'),
+(3, 'bouche-02.png'),
+(4, 'bouche-03.png'),
+(5, 'bouche-04.png'),
+(6, 'bouche-05.png'),
+(7, 'bouche-06.png'),
+(8, 'bouche-07.png'),
+(9, 'bouche-08.png'),
+(10, 'bouche-09.png'),
+(11, 'bouche-10.png'),
+(12, 'bouche-11.png'),
+(13, 'bouche-12.png');
 
 -- --------------------------------------------------------
 
@@ -37,14 +57,22 @@ CREATE TABLE `bouche` (
 -- Structure de la table `ensemble`
 --
 
-CREATE TABLE `ensemble` (
-  `id_ensemble` int(11) NOT NULL,
-  `id_visage` int(11) NOT NULL,
-  `id_nez` int(11) NOT NULL,
-  `id_bouche` int(11) NOT NULL,
-  `id_yeux` int(11) NOT NULL,
-  `nom` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `ensemble` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(200) NOT NULL,
+  `ensemble` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Contenu de la table `ensemble`
+--
+
+INSERT INTO `ensemble` (`id`, `nom`, `ensemble`) VALUES
+(2, 'Eric', 'face-04.png,eyes-04.png,nez-03.png,bouche-04.png'),
+(3, 'Patrick', 'face-00.png,eyes-00.png,nez-00.png,bouche-00.png'),
+(5, 'heyhey', 'face-01.png,eyes-04.png,nez-06.png,bouche-04.png'),
+(7, 'tiboulet', 'face-08.png,eyes-09.png,nez-09.png,bouche-10.png');
 
 -- --------------------------------------------------------
 
@@ -52,10 +80,30 @@ CREATE TABLE `ensemble` (
 -- Structure de la table `nez`
 --
 
-CREATE TABLE `nez` (
-  `id_nez` int(11) NOT NULL,
-  `img_nez` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `nez` (
+  `id_nez` int(11) NOT NULL AUTO_INCREMENT,
+  `img_nez` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_nez`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=275 ;
+
+--
+-- Contenu de la table `nez`
+--
+
+INSERT INTO `nez` (`id_nez`, `img_nez`) VALUES
+(1, 'nez-00.png'),
+(2, 'nez-01.png'),
+(3, 'nez-02.png'),
+(4, 'nez-03.png'),
+(5, 'nez-04.png'),
+(6, 'nez-05.png'),
+(7, 'nez-06.png'),
+(8, 'nez-07.png'),
+(9, 'nez-08.png'),
+(10, 'nez-09.png'),
+(11, 'nez-10.png'),
+(12, 'nez-11.png'),
+(13, 'nez-12.png');
 
 -- --------------------------------------------------------
 
@@ -63,10 +111,30 @@ CREATE TABLE `nez` (
 -- Structure de la table `visage`
 --
 
-CREATE TABLE `visage` (
-  `id_visage` int(11) NOT NULL,
-  `img_visage` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `visage` (
+  `id_visage` int(11) NOT NULL AUTO_INCREMENT,
+  `img_visage` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_visage`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=274 ;
+
+--
+-- Contenu de la table `visage`
+--
+
+INSERT INTO `visage` (`id_visage`, `img_visage`) VALUES
+(1, 'face-00.png'),
+(2, 'face-01.png'),
+(3, 'face-02.png'),
+(4, 'face-03.png'),
+(5, 'face-04.png'),
+(6, 'face-05.png'),
+(7, 'face-06.png'),
+(8, 'face-07.png'),
+(9, 'face-08.png'),
+(10, 'face-09.png'),
+(11, 'face-10.png'),
+(12, 'face-11.png'),
+(13, 'face-12.png');
 
 -- --------------------------------------------------------
 
@@ -74,75 +142,31 @@ CREATE TABLE `visage` (
 -- Structure de la table `yeux`
 --
 
-CREATE TABLE `yeux` (
-  `id_yeux` int(11) NOT NULL,
-  `img_yeux` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `yeux` (
+  `id_yeux` int(11) NOT NULL AUTO_INCREMENT,
+  `img_yeux` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_yeux`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=274 ;
 
 --
--- Index pour les tables exportées
+-- Contenu de la table `yeux`
 --
 
---
--- Index pour la table `bouche`
---
-ALTER TABLE `bouche`
-  ADD PRIMARY KEY (`id_bouche`);
+INSERT INTO `yeux` (`id_yeux`, `img_yeux`) VALUES
+(1, 'eyes-00.png'),
+(2, 'eyes-01.png'),
+(3, 'eyes-02.png'),
+(4, 'eyes-03.png'),
+(5, 'eyes-04.png'),
+(6, 'eyes-05.png'),
+(7, 'eyes-06.png'),
+(8, 'eyes-07.png'),
+(9, 'eyes-08.png'),
+(10, 'eyes-09.png'),
+(11, 'eyes-10.png'),
+(12, 'eyes-11.png'),
+(13, 'eyes-12.png');
 
---
--- Index pour la table `ensemble`
---
-ALTER TABLE `ensemble`
-  ADD PRIMARY KEY (`id_ensemble`),
-  ADD KEY `id_visage` (`id_visage`,`id_nez`,`id_bouche`,`id_yeux`);
-
---
--- Index pour la table `nez`
---
-ALTER TABLE `nez`
-  ADD PRIMARY KEY (`id_nez`);
-
---
--- Index pour la table `visage`
---
-ALTER TABLE `visage`
-  ADD PRIMARY KEY (`id_visage`);
-
---
--- Index pour la table `yeux`
---
-ALTER TABLE `yeux`
-  ADD PRIMARY KEY (`id_yeux`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `bouche`
---
-ALTER TABLE `bouche`
-  MODIFY `id_bouche` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `ensemble`
---
-ALTER TABLE `ensemble`
-  MODIFY `id_ensemble` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `nez`
---
-ALTER TABLE `nez`
-  MODIFY `id_nez` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `visage`
---
-ALTER TABLE `visage`
-  MODIFY `id_visage` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `yeux`
---
-ALTER TABLE `yeux`
-  MODIFY `id_yeux` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
